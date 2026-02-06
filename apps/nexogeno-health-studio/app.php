4816d5e8edb8b91459d69837c3ea8f0323b4c7a6
Eduardo Plustag <eduardo@plustag.com.br>
2026-02-06 14:57:46 -0300

    Nexogeno Care <> Health Studio

--- apps/nexogeno-health-studio/app.php
+++ apps/nexogeno-health-studio/app.php
@@ -1,13 +1,13 @@
 <?php
 
 return array(
-	'id'            => 'nexogeno-care',
-	'name'          => 'NexoGENO Care',
+	'id'            => 'nexogeno-health-studio',
+	'name'          => 'NexoGENO Health Studio',
 	'version'       => '0.1.0',
 	'enabled'       => true,
-	'route'         => 'app-nexogeno-care',
+	'route'         => 'app-nexogeno-health-studio',
 	'query_var'     => 'nexogeno_app',
-	'query_value'   => 'care',
+	'query_value'   => 'health-studio',
 	'products'      => array(4870),
 	'templates_dir' => __DIR__ . '/templates',
 	'template'      => __DIR__ . '/templates/app.php',
